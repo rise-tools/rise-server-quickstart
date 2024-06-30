7e83af049df414ecc9e5a35c26b730fea3b6fd1d
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2024-06-30 21:44:08 +0530

    feat: Add Components for Dynamic Expo Router Navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,36 @@
-import { createWSServer, state, view } from '@rise-tools/server'
-import { YStack, Text, Button } from '@rise-tools/kitchen-sink/server'
+import { goBack, navigate } from '@rise-tools/kit-expo-router/server';
+import { Button, Text, YStack } from '@rise-tools/kitchen-sink/server';
+import { createWSServer, state, view } from '@rise-tools/server';
 
-const [count, setCount] = state(0)
+const [count, setCount] = state(0);
 
-const incrementer = view(get => (
-    <YStack>
-        <Text>The count is {get(count)}</Text>
-        <Button onPress={() => {
-            setCount(c => c + 1)
-        }}>
-            Plus 1
-        </Button>
-    </YStack>
-))
+const incrementer = view((get) => (
+  <YStack>
+    <Text>The count is {get(count)}</Text>
+    <Button
+      onPress={() => {
+        setCount((c) => c + 1);
+      }}
+    >
+      Plus 1
+    </Button>
+  </YStack>
+));
 
-console.log('Starting server on ws://localhost:8888')
+const home = view(() => (
+  <YStack>
+    <Text>{'Home From Server'}</Text>
+    <Button onPress={navigate('details')}>Go to Details</Button>
+  </YStack>
+));
 
-createWSServer({ incrementer }, 8888)
+const details = view(() => (
+  <YStack>
+    <Text>{'Details From Server'}</Text>
+    <Button onPress={goBack()}>Go Back</Button>
+  </YStack>
+));
+
+console.log('Starting server on ws://localhost:8888');
+
+createWSServer({ incrementer, home, details }, 8888);
